4dd7b34a0fb166493693ea9fed99500a9c19bb45
DELL <aycholpon25t@gmail.com>
2023-03-19 17:14:53 +0600

    first

--- app/src/main/java/com/example/gitbranches/MainActivity.kt
+++ app/src/main/java/com/example/gitbranches/MainActivity.kt
@@ -5,10 +5,12 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     val a = 2
+    //date
     val b = 4
     var res = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val aiko = 2005
         setContentView(R.layout.activity_main)
         res = a + b
     }
