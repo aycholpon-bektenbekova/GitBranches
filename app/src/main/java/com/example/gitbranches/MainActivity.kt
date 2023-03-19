cacfb29cae49150ad5eb821336c74c5a519c2b5d
DELL <aycholpon25t@gmail.com>
2023-03-19 16:36:48 +0600

    sum

--- app/src/main/java/com/example/gitbranches/MainActivity.kt
+++ app/src/main/java/com/example/gitbranches/MainActivity.kt
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    val a = 2
+    val b = 4
+    var res = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        res = a + b
     }
 }
